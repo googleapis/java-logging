d460356d8359bed8e3f8a7059fd77a40ec385418
minherz <minherz@gmail.com>
2021-07-30 11:14:03 +0000

    chore: set generated serialVersionUID for LogNamePageFetcher

--- google-cloud-logging/src/main/java/com/google/cloud/logging/LoggingImpl.java
+++ google-cloud-logging/src/main/java/com/google/cloud/logging/LoggingImpl.java
@@ -199,7 +199,7 @@ class LoggingImpl extends BaseService<LoggingOptions> implements Logging {
 
   private static class LogNamePageFetcher extends BasePageFetcher<String> {
 
-    private static final long serialVersionUID = 5L;
+    private static final long serialVersionUID = 7156613795615829594L;
 
     LogNamePageFetcher(
         LoggingOptions serviceOptions, String cursor, Map<Option.OptionType, ?> requestOptions) {
