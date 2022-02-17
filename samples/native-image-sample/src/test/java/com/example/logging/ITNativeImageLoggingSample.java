8cf2024a2ce857593efa4e3c885890649ffd568c
Mridula Peddada <mpeddada@google.com>
2022-02-16 22:07:47 -0500

    docs(sample): Add sample for Native Image support

---
+++ samples/native-image-sample/src/test/java/com/example/logging/ITNativeImageLoggingSample.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.logging;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.logging.Logging;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ITNativeImageLoggingSample {
+
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+  private static Logging logging;
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @Test
+  public void testLogging() {
+    bout.reset();
+    NativeImageLoggingSample.main(new String[]{});
+    String output = bout.toString();
+    assertThat(output).contains("Logged: This is a log produced by Native Image.");
+  }
+}
