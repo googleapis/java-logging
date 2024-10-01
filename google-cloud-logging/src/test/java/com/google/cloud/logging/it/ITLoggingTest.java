691111205d1686e43248d5b52ef6616d223f1612
Lawrence Qiu <lawrenceqiu@google.com>
2024-10-01 15:53:50 -0400

    chore: Temporarily disable ITLoggingTest.testListEntries()

--- google-cloud-logging/src/test/java/com/google/cloud/logging/it/ITLoggingTest.java
+++ google-cloud-logging/src/test/java/com/google/cloud/logging/it/ITLoggingTest.java
@@ -32,6 +32,7 @@ import com.google.logging.v2.LogName;
 import java.util.Iterator;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ITLoggingTest extends BaseSystemTest {
@@ -82,6 +83,7 @@ public class ITLoggingTest extends BaseSystemTest {
     assertTrue(cleanupLog(LOG_ID));
   }
 
+  @Ignore
   @Test(timeout = 600_000) // Note: it can take ~10 minutes for logs to propagate!
   public void testListEntries() throws InterruptedException {
     LoggingOptions loggingOptions = logging.getOptions();
